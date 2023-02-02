fcc0e8f638d8050f77534b647cb6fd3c6bbd3e3d
glassman2015 <jadeng817@aol.com>
2023-02-02 15:31:44 -0500

    FastAPI

--- CSC230-projects/main.py
+++ CSC230-projects/main.py
@@ -1,15 +1,23 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    password: str
+    email: str
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/{item_name}")
+def read_item(item_name: str, item_email: str, item_password: str):
+    return {"item_name": item_name, "item_email": item_email, "item_password": item_password}
+
+@app.put("/items/{item_name}")
+def update_item(item_name: str, item_email: str, item_password: str, item: Item):
+    return {"item_name": item.name, "item_email": item.email, "item_password": item.password}
