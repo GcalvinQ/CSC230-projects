8b5cd11a9842e8ab1a2742459d5dabf14863d781
glassman2015 <jadeng817@aol.com>
2023-02-16 17:41:13 -0500

    Updated all of django

--- django_projects/locallibrary/locallibrary/urls.py
+++ django_projects/locallibrary/locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""backend URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -19,3 +19,21 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Use include() to add paths from the catalog application
+from django.urls import include
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='catalog/', permanent=True)),
+]
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
