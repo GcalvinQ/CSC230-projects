7f43d8079efee3337e151011d3eabd8d9640dd3b
Jaden Greenwald <jadeng817@aol.com>
2023-05-01 16:47:25 -0400

    Delete makerspace directory

--- makerspace/website/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Account
-
-# Register your models here.
-class AccountAdmin(admin.ModelAdmin):
-    list_display = ['firstname', 'lastname', 'email', 'username']
-admin.site.register(Account, AccountAdmin)
