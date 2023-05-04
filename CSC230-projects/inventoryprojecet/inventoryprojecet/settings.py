9faf4a591630cc6fca20f78b095b9f6113638180
glassman2015 <jadeng817@aol.com>
2023-05-04 13:21:31 -0400

    Turned debug off

--- CSC230-projects/inventoryprojecet/inventoryprojecet/settings.py
+++ CSC230-projects/inventoryprojecet/inventoryprojecet/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q$4c&9!m5r#12i9kc1nldw@vc9podse#(k@0twy+q1d7+943vj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
